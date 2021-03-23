32f41b4aba00331468784415e9cf1dc27dee6809
Cameron Makin <cammakin8@gmail.com>
2021-03-23 12:57:13 -0400

    Added socketio and cmdline args

--- Core/Simulator.py
+++ Core/Simulator.py
@@ -1,9 +1,50 @@
 from datetime import datetime, timedelta
 from math import radians, cos, sin, asin, sqrt
 import random
+import socketio
+from argparse import ArgumentParser # If we want command line args
+
+
+# Parser extracts command line flags/parameters  
+parser = ArgumentParser(description='Testbench-to-SAS Client Script - Provide a server address and port in order to connect to the SAS.')
+
+# Socket Params----------------------------------------------------------------------------
+parser.add_argument('-a','--address',\
+		help='Server address. Example: -a \'127.0.0.1\'',\
+		default='localhost')
+parser.add_argument('-p','--port',\
+		help='Server port. Example: -p 65432',\
+		default='8000')
+#------------------------------------------------------------------------------------------
+
+# Simulation Args --------------------------------------------------------------------------
+parser.add_argument('--numberOfUsers',default=10)
+parser.add_argument('--percentageMU',default=15)
+parser.add_argument('--varianceOfData',default=5)
+parser.add_argument('--percentageMobile',default=10)
+parser.add_argument('--secureCount',default=1)
+#------------------------------------------------------------------------------------------
+
+class User:
+    def __init__(self, id, isMU, percentageMU, latitude, longitude, secure, isMobile=False):
+        self.id = id
+        self.isMU = isMU
+        self.percentageMU = percentageMU
+        self.latitude = latitude
+        self.longitude = longitude
+        self.secure = secure
+        self.mobile = isMobile
+
 
 class Simulator:
     def __init__(self, numberOfUsers, percentageMU, varianceOfData, percentageMobile, secureCount):
+        """
+        numberOfUsers (int) - how many users will be connecting to the SAS
+        percentageMU (int [0,100]) - what percent chance of a user will be malicious
+        varianceOfData - 
+        percentageMobile - (does this include MUs?)
+        secureCount - 
+        """
         self.users = []
         self.numberOfUsers = numberOfUsers
         self.varianceOfData = varianceOfData
@@ -18,8 +59,8 @@ class Simulator:
 
         #assumes this is for one frequency range
 
-        numberMU = (numberOfUsers*percentageMU)/100
-        mobile = False
+        numberMU = numberOfUsers*(percentageMU/100)
+        # mobile = False
         sc = 0
         for x in range(numberOfUsers):
             if self.percentageMobile < random.randrange(0, 100):
@@ -27,18 +68,19 @@ class Simulator:
             else:
                 mobile = False
             if x < numberMU:#malicious node
-                createUser("", True, self.percentageMU, self.generateLat(), self.generateLon, False, mobile)
+                self.createUser("", True, self.percentageMU, self.generateLat(), self.generateLon(), False, mobile)
             else:
                 if sc < self.secureCount:#is secure
-                    createUser("", False, 0, self.generateLat(), self.generateLon, True, False)
+                    self.createUser("", False, 0, self.generateLat(), self.generateLon(), True, False)
+                    sc = sc + 1 # Right?
                 else:#regular node
-                    createUser("", False, 0, self.generateLat(), self.generateLon, False, mobile)
+                    self.createUser("", False, 0, self.generateLat(), self.generateLon(), False, mobile)
 
 
 
 
-    def createUser(self, id, isMU, percentageMU, latitude, longitude):
-        self.users.append(User("", isMU, percentageMU, latitude, longitude))
+    def createUser(self, id, isMU, percentageMU, latitude, longitude, secure, isMobile):
+        self.users.append(User("", isMU, percentageMU, latitude, longitude, secure, isMobile))
 
 
     def generateLat(self):
@@ -52,7 +94,7 @@ class Simulator:
 
     def makeGoodData(self):
         arr = []
-        for x in range(self.arraySize):
+        for _ in range(self.arraySize):
             arr.append(self.makeData(self.goodLow, self.goodHigh))
         return arr
 
@@ -63,7 +105,7 @@ class Simulator:
 
     def makePUData(self):
         arr = []
-        for x in range(self.arraySize):
+        for _ in range(self.arraySize):
             arr.append(self.makeData(self.PULow, self.PUHigh))
         return arr
 
@@ -94,8 +136,6 @@ class Simulator:
                     sendData(user, self.makeGoodData()())
 
 
-
-
     def sendData(self, user, data):
         print("todo")
 
@@ -106,12 +146,121 @@ class Simulator:
                 user.longitude = self.generateLon()
 
 
-class User:
-    def __init__(self, id, isMU, percentageMU, latitude, longitude, secure, isMobile):
-        self.id = id
-        self.isMU = isMU
-        self.percentageMU = percentageMU
-        self.latitude = latitude
-        self.longitude = longitude
-        self.secure = secure
-        self.mobile = mobile
+def defineSocketEvents(clientio):
+	"""
+	List of events the SAS may emit, and functions to call to handle them
+
+	Parameters
+	----------
+	clientio : socketio Client object
+		socket to SAS
+	"""
+	@clientio.event
+	def connect():
+		print('Socket connection established! Given sid: ' + clientio.sid)
+
+	# @clientio.event
+	# def identifySource():
+	# 	clientio.emit("identifySource", ("I am CRTS"))
+
+	# Official WinnForum Predefined Functionality
+	@clientio.event
+	def registrationResponse(data):
+		if(nodes_awaiting_response):
+			handleRegistrationResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Registration Response from SAS.")
+
+	@clientio.event
+	def spectrumInquiryResponse(data):
+		if(nodes_awaiting_response):
+			handleSpectrumInquiryResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Spectrum Inquiry Response from SAS.")
+
+	@clientio.event
+	def grantResponse(data):
+		if(nodes_awaiting_response):
+			handleGrantResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Grant Response from SAS.")
+
+	@clientio.event
+	def heartbeatResponse( data):
+		if(nodes_awaiting_response):
+			handleHeartbeatResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Heartbeat Response from SAS.")
+
+	@clientio.event
+	def relinquishmentResponse(data):
+		if(nodes_awaiting_response):		
+			handleRelinquishmentResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Relinquishment  Response from SAS.")
+
+	@clientio.event
+	def deregistrationResponse(data):
+		if(nodes_awaiting_response):
+			handleDeregistrationResponse(clientio, data)
+			global __blocked
+			__blocked = False
+		else:
+			print("No Nodes are awaiting a SAS response. Ignoring Deregistration Response from SAS.")
+	# end official WinnForum functions
+
+	@clientio.event
+	def changeRadioParams(data):
+		"""
+		SAS command to change RX Parameters for a Node with a cbsdId.
+		Calls 'updateRxParams(data)'
+
+		Parameters
+		----------
+		data : dictonary
+			Expected keys are cbsdId, highFreq, and lowFreq
+		"""
+		updateRxParams(data)
+
+	@clientio.event
+	def disconnect():
+		"""
+		SAS Command to tell the socket connection to close
+		"""
+		print('Connection to SAS terminating')
+		print("Exiting System...")
+		sys.exit()
+
+
+def main(args):
+    clientio = socketio.Client()  # Create Client Socket
+    defineSocketEvents(clientio)  # Define handlers for events the SAS may emit
+    socket_addr = 'http://' + args['address'] +':' + args['port']
+    clientio.connect(socket_addr) # Connect to SAS
+
+    # Pulls values for Simulation from commandline
+    # Top of this script allows you to set default values for these
+    numberOfUsers = int(args['numberOfUsers'])
+    percentageMU = float(args['percentageMU'])
+    varianceOfData = float(args['varianceOfData'])
+    percentageMobile = float(args['percentageMobile'])
+    secureCount = float(args['secureCount']) # or int?
+
+    sim_one = Simulator(numberOfUsers, percentageMU, varianceOfData, percentageMobile, secureCount)
+
+    # Insert gracfully exit implementation
+
+
+if __name__ == "__main__":
+    args = vars(parser.parse_args())	# Get command line arguments
+    main(args)
