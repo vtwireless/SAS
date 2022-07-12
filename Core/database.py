8cd85d61ca5c2813fffbc28da9754522ad2f4447
sauravk3 <sauravk3@vt.edu>
2022-07-12 18:39:10 -0400

    backend node and user db hooks added

--- Core/database.py
+++ Core/database.py
@@ -7,20 +7,17 @@ USER = "root"
 PASSWORD = "root"
 DATABASE = "spectrumGrant"
 
-# Define Tables
-SULOGIN = "secondaryuser"
-ADMINLOGIN = "admin"
-NODES = "node"
+USERTABLE = "secondaryuser"
+ADMINTABLE = "admin"
+NODETABLE = "node"
 
-dbconnect = MySQLdb.connect(
-    HOST,
-    USER,
-    PASSWORD,
-    DATABASE
-)
-
+# TODO: Database connection object should be created once and then supplied elsewhere. In the current
+#  implementation, a new connection object is getting created everytime database.py is executed. Due
+#  to this, if DB is updated via CLI we don't see changes in UI until FLASK is restarted.
+dbconnect = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE)
 dbcursor = dbconnect.cursor(cursorclass=DictCursor)
 
+# --- HELPER
 
 def formatReturnable(status, payload):
     if status == 0:
@@ -32,6 +29,25 @@ def formatReturnable(status, payload):
         return payload
 
 
+def checkIfExists(tableName, args: dict):
+    if not args:
+        return False
+
+    args_list = ["=".join([key, f"'{value}'"]) for key, value in args.items()]
+    query = f"SELECT * FROM {tableName} WHERE " + " AND ".join(args_list)
+
+    dbcursor.execute(query)
+    queryResults = dbcursor.fetchone()
+
+    if queryResults:
+        return True
+
+    return False
+
+
+# --- USER REGISTRATION & ACCESS FUNCTIONALITIES ---
+
+
 def secondaryUserLogin(payload):
     username = payload['username']
     password = payload['password']
@@ -39,7 +55,7 @@ def secondaryUserLogin(payload):
     if not username or not password:
         raise Exception("Username or password not provided")
 
-    query = f"SELECT *  FROM {SULOGIN} WHERE secondaryUserEmail = '{username}' AND secondaryUserPassword = '{password}'"
+    query = f"SELECT *  FROM {USERTABLE} WHERE secondaryUserEmail = '{username}' AND secondaryUserPassword = '{password}'"
     dbcursor.execute(query)
     queryResults = list(dbcursor.fetchall())
 
@@ -61,7 +77,7 @@ def adminLogin(payload):
     if not username or not password:
         raise Exception("Username or password not provided")
 
-    query = f"SELECT *  FROM {ADMINLOGIN} WHERE adminEmail = '{username}' AND adminPassword = '{password}'"
+    query = f"SELECT *  FROM {ADMINTABLE} WHERE adminEmail = '{username}' AND adminPassword = '{password}'"
     dbcursor.execute(query)
     queryResults = list(dbcursor.fetchall())
 
@@ -76,22 +92,119 @@ def adminLogin(payload):
         return formatReturnable(1, response)
 
 
+def createSU(payload):
+    secondaryUserName = payload['secondaryUserName']
+    secondaryUserEmail = payload['secondaryUserEmail']
+    secondaryUserPassword = payload['secondaryUserPassword']
+    deviceID = payload['deviceID']
+    location = payload['location']
+
+    # Sanity Checks
+    if not secondaryUserName:
+        raise Exception("Username not provided")
+
+    if not secondaryUserEmail:
+        raise Exception("Email Address not provided")
+
+    if not secondaryUserPassword:
+        raise Exception("Password not provided")
+
+    if checkIfExists(USERTABLE, {'secondaryUserEmail': secondaryUserEmail}):
+        message = f"Email '{secondaryUserEmail}' already exists. Contact an administrator to recover or reset password."
+        return formatReturnable(0, {"exists": 1, "message":message})
+
+    # Create User
+    query = f"INSERT INTO {USERTABLE} (secondaryUserEmail, secondaryUserPassword, secondaryUserName, deviceID, " \
+            f"location, tier) VALUES ('{secondaryUserEmail}', '{secondaryUserPassword}', '{secondaryUserName}', " \
+            f"'{deviceID}', '{location}', '3');"
+    dbcursor.execute(query)
+    dbconnect.commit()
+
+    if not checkIfExists(USERTABLE, {'secondaryUserEmail': secondaryUserEmail}):
+        message = "Secondary User could not be added. Contact an administrator."
+        return formatReturnable(0, {"message": message})
+
+    return formatReturnable(1, {"message": "Secondary User has been added."})
+
+def checkEmailAvailability(payload):
+    email = payload['email']
+
+    if not email:
+        raise Exception('Email Address not provided')
+
+    if checkIfExists(USERTABLE, {"secondaryUserEmail": email}):
+        return formatReturnable(1, {"exists": 1, "message": "Email Address already in use."})
+
+    return formatReturnable(1, {"exists": 0, "message": "Email Address not in use."})
+
+# --- NODE MANAGEMENT ROUTES ---
+
 def getNodes():
-    query = f'SELECT * FROM {NODES}'
+    query = f'SELECT * FROM {NODETABLE}'
     dbcursor.execute(query)
     queryResults = list(dbcursor.fetchall())
+    print(queryResults)
 
     return formatReturnable(1, {
         'nodes': queryResults
     })
 
+def createNode(payload):
+    nodeName = payload.get('nodeName', '')
+    location = payload.get('location', '')
+    SUID = payload.get('SUID', '')
+    trustLevel = int(payload.get('trustLevel', '5'))
+    IPAddress = payload.get('IPAddress', '')
+    minFrequency = float(payload.get('minFrequency', '0'))
+    maxFrequency = float(payload.get('maxFrequency', '0'))
+    minSampleRate = float(payload.get('minSampleRate', '0'))
+    maxSampleRate = float(payload.get('maxSampleRate', '0'))
+    nodeType = payload.get('nodeType', '')
+    status = payload.get('status', '')
+    comment = payload.get('comment', '')
+    mobility = payload.get('mobility', 'False').lower()
+    if mobility == 'false':
+        mobility = 0
+    else:
+        mobility = 1
 
-# TODO: Only for testing. Remove thereafter.
-if __name__ == '__main__':
-    # Login Functionality
-    loginPayload = {
-        'username': "fake@fake",
-        'password': "pass"
-    }
-    print(secondaryUserLogin(loginPayload))
+    userId = payload.get('userId', '')
+    fccId = payload.get('fccId', '')
+    cbsdSerialNumber = payload.get('cbsdSerialNumber', '')
+    callSign = payload.get('callSign', '')
+    cbsdCategory = payload.get('cbsdCategory', '')
+    cbsdInfo = payload.get('cbsdInfo', '')
+    airInterface = payload.get('airInterface', '')
+    installationParam = payload.get('installationParam', '')
+    measCapability = payload.get('measCapability', '')
+    groupingParam = payload.get('groupingParam', '')
+
+    # Sanity Check
+    if checkIfExists(NODETABLE, {'nodeName': nodeName}):
+        message = "A node already exists with the same name"
+        return formatReturnable(0, {"exists": 1, "message":message})
+
+    # Create Node
+    # TODO: Extra Params are currently missing from implementation
+    # query = f"INSERT INTO {NODETABLE} (nodeName, secondaryUserId, location, trustLevel, IPAddress, minFrequency, " \
+    #         f"maxFrequency, minSampleRate, maxSampleRate, nodeType, mobility, status, comment, fccId, " \
+    #         f"cbsdSerialNumber, callSign, cbsdCategory, cbsdInfo, airInterface, installationParam, measCapability, " \
+    #         f"groupingParam) VALUES ('{nodeName}', '{SUID}', '{location}', '{trustLevel}', '{IPAddress}', " \
+    #         f"'{minFrequency}', '{maxFrequency}', '{minSampleRate}', '{maxSampleRate}', '{nodeType}', '{mobility}', " \
+    #         f"'{status}', '{comment}', '{fccId}', '{cbsdSerialNumber}', '{callSign}', '{cbsdCategory}', '{cbsdInfo}'," \
+    #         f" '{airInterface}', '{installationParam}', '{measCapability}', '{groupingParam}');"
+
+    query = f"INSERT INTO {NODETABLE} (nodeName, location, trustLevel, IPAddress, minFrequency, maxFrequency, " \
+            f"minSampleRate, maxSampleRate, nodeType, mobility, status, comment) VALUES ('{nodeName}', '{location}'," \
+            f" '{trustLevel}', '{IPAddress}', '{minFrequency}', '{maxFrequency}', '{minSampleRate}', " \
+            f"'{maxSampleRate}', '{nodeType}', '{mobility}', '{status}', '{comment}');"
+
+    dbcursor.execute(query)
+    dbconnect.commit()
+
+    if not checkIfExists(NODETABLE, {'nodeName': nodeName}):
+        message = "Node could not be added. Contact an administrator."
+        return formatReturnable(0, {"message": message})
+
+    return formatReturnable(1, {"message": "Node has been added."})
 
