1bb6a0efed2be54fb8a1fcb3fcd11731c7ce60a9
sauravk3 <sauravk3@vt.edu>
2022-09-14 14:36:49 -0400

    clean up

--- swift/swiftascent/core/models.py
+++ swift/swiftascent/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class PrioritySchema(models.Model):
-    category_type = models.CharField(max_length=255)
-    category_values = models.CharField(max_length=255)
-    priority_index = models.CharField(max_length=255)
+    category_type = models.TextField()
+    category_values = models.TextField()
+    priority_index = models.TextField()
 
