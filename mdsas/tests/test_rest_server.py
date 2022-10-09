1cd0238fca6c109bfa4c161175352f2fe91456a6
sauravk3 <sauravk3@vt.edu>
2022-10-09 00:52:51 -0400

    test framework refactoring

--- mdsas/tests/test_rest_server.py
+++ mdsas/tests/test_rest_server.py
@@ -1,57 +1,41 @@
 import json
 import requests
+from enum import Enum
+
+POST_HEADERS, GET_HEADERS = {'Content-Type': 'application/json'}, {}
+with open('test_data.json', 'r+') as infile:
+    payloads = json.load(infile)
+
+
+class HttpMethod(Enum):
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    PATCH = 'PATCH'
+
+
+def send_request_to_server(method: HttpMethod, url: str):
+    URL = "http://localhost:8000/" + url
+    response = None
+    if method == HttpMethod.GET:
+        response = requests.request("GET", URL, headers=GET_HEADERS, data={})
+
+    elif method == HttpMethod.POST:
+        response = requests.request("POST", URL, headers=POST_HEADERS, data=json.dumps(payloads[url]))
+
+    return response
 
 
 def test_getUsers():
-    url = "http://localhost:8000/getUsers"
-
-    payload = {}
-    headers = {}
-
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    # print(response.text)
+    response = send_request_to_server(HttpMethod.GET, 'getUsers')
     assert response.json()['status'] == 1
 
 
 def test_getUser():
-    url = "http://localhost:8000/getUser"
-
-    payload = json.dumps({
-        "username": "abc@abc.com",
-        "password": "password"
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    # print(response.text)
+    response = send_request_to_server(HttpMethod.POST, 'getUser')
     assert response.json()['status'] == 1
 
 
 def test_spectrumInquiry():
-    url = "http://localhost:8000/spectrumInquiryRequest"
-
-    payload = json.dumps({
-        "spectrumInquiryRequest": [
-            {
-                "cbsdId": 1,
-                "inquiredSpectrum": [
-                    {
-                        "lowFrequency": 3550000000,
-                        "highFrequency": 3560000000
-                    }
-                ]
-            }
-        ]
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    # print(response.text)
+    response = send_request_to_server(HttpMethod.POST, 'spectrumInquiryRequest')
     assert response.json()['status'] == 1
