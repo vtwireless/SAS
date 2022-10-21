de7d7a9a07c9f15f5f04d17f740df4df38fd8733
sauravk3 <sauravk3@vt.edu>
2022-10-20 23:32:29 -0400

    test cases added for spectrum inquiry

--- mdsas/tests/test_rest_server.py
+++ mdsas/tests/test_rest_server.py
@@ -16,38 +16,45 @@ class HttpMethod(Enum):
     PATCH = 'PATCH'
 
 
-def send_request_to_server(method: HttpMethod, url: str):
-    URL = f"http://{HOST}:{PORT}/" + url
-    response = None
-    if method == HttpMethod.GET:
-        response = requests.request("GET", URL, headers=GET_HEADERS, data={})
+class TestRestServer:
+    
+    @staticmethod
+    def send_request_to_server(method: HttpMethod, url: str):
+        URL = f"http://{HOST}:{PORT}/" + url
+        response = None
+        if method == HttpMethod.GET:
+            response = requests.request("GET", URL, headers=GET_HEADERS, data={})
 
-    elif method == HttpMethod.POST:
-        response = requests.request("POST", URL, headers=POST_HEADERS, data=json.dumps(payloads[url]))
+        elif method == HttpMethod.POST:
+            response = requests.request("POST", URL, headers=POST_HEADERS, data=json.dumps(payloads[url]))
 
-    return response
+        return response
+    
+    def test_getUsers(self):
+        response = self.send_request_to_server(HttpMethod.GET, 'getUsers')
+        assert response.json()['status'] == 1
 
+    def test_getUser(self):
+        response = self.send_request_to_server(HttpMethod.POST, 'getUser')
+        assert response.json()['status'] == 1
 
-def test_getUsers():
-    response = send_request_to_server(HttpMethod.GET, 'getUsers')
-    assert response.json()['status'] == 1
+    def test_spectrumInquiry(self):
+        response = self.send_request_to_server(HttpMethod.POST, 'spectrumInquiryRequest')
+        response_json = response.json()
 
+        assert response_json['status'] == 1
+        assert "spectrumInquiryResponse" in response_json
+        assert len(response_json["spectrumInquiryResponse"]) == 2
+        for response_item in response_json["spectrumInquiryResponse"]:
+            assert "cbsdId" in response_item
+            assert "response" in response_item
+            assert "responseCode" in response_item["response"]
+            assert "responseMessage" in response_item["response"]
 
-def test_getUser():
-    response = send_request_to_server(HttpMethod.POST, 'getUser')
-    assert response.json()['status'] == 1
+    def test_suLogin(self):
+        response = self.send_request_to_server(HttpMethod.POST, 'suLogin')
+        assert response.json()['status'] == 1
 
-
-def test_spectrumInquiry():
-    response = send_request_to_server(HttpMethod.POST, 'spectrumInquiryRequest')
-    assert response.json()['status'] == 1
-
-def test_suLogin():
-    response = send_request_to_server(HttpMethod.POST, 'suLogin')
-    assert response.json()['status'] == 1
-
-def test_adminLogin():
-    response = send_request_to_server(HttpMethod.POST, 'adminLogin')
-    assert response.json()['status'] == 1
-
-
+    def test_adminLogin(self):
+        response = self.send_request_to_server(HttpMethod.POST, 'adminLogin')
+        assert response.json()['status'] == 1
