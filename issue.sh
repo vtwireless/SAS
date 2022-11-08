f23d8db42c29e509c4e8785ebfe9a8859b053d1a
sauravk3 <sauravk3@vt.edu>
2022-11-07 23:47:32 -0500

    Helper file for creating issues

---
+++ issue.sh
@@ -0,0 +1,6 @@
+gh issue create \
+--title "Test Case Issue: Check Email Availability when user exists" \
+--body "Response indicates that email is available which should not be the case" \
+--label "bug" \
+--assignee "@me" \
+--project "CCI-MDSAS"
