8bb432762d0ab24b9dbc43e923b71a0c23a9b267
sauravk3 <sauravk3@vt.edu>
2023-03-06 13:44:43 -0500

    nrdz demo usecase

--- mdsas/tests/nrdz_demo.py
+++ mdsas/tests/nrdz_demo.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
     logger = logging.getLogger("nrdz_demo")
     logger.setLevel(logging.DEBUG)
 
-    # logger.info("Creating a new user")
-    # res = client.send_request_to_server(HttpMethod.POST, 'createSU')
-    # print(json.dumps(res.json(), indent=4))
-    #
-    # logger.info("Adding 3 nodes [2 at HCRO and 1 at BB]")
-    # res = client.send_request_to_server(HttpMethod.POST, 'registrationRequest')
-    # print(json.dumps(res.json(), indent=4))
+    logger.info("Creating a new user")
+    res = client.send_request_to_server(HttpMethod.POST, 'createSU')
+    print(json.dumps(res.json(), indent=4))
+
+    logger.info("Adding 3 nodes [2 at HCRO and 1 at BB]")
+    res = client.send_request_to_server(HttpMethod.POST, 'registrationRequest')
+    print(json.dumps(res.json(), indent=4))
 
     logger.info("Sending Grant Requests")
     res = client.send_request_to_server(HttpMethod.POST, 'grantRequest')
